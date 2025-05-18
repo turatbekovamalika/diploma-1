76217ad5c48c30bf3026cef9907ef3dee4ae23db
Turatbekova Malika <m18049687>
2025-05-18 14:07:50 +0600

    add route path "delivery" for app js

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ export default function App() {
           <Route path="/" element={<h2>Home</h2>} />
           <Route path="/about" element={<h2>About</h2>} />
           <Route path="/contacts" element={<h2>Contacts</h2>} />
+          <Route path="/delivery" element={<h2>Delivery</h2>} />
         </Routes>
       </Layout>
     </div>
