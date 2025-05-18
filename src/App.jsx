493fff93652d63294c4d14a7d673968182fb8ac2
Turatbekova Malika <m18049687>
2025-05-18 14:15:08 +0600

    created the home components with the content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
-import "./App.css";
+import Home from "./pages/Home";
 export default function App() {
   return (
     <div className="App">
       <Layout>
         <Routes>
-          <Route path="/" element={<h2>Home</h2>} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<h2>About</h2>} />
           <Route path="/contacts" element={<h2>Contacts</h2>} />
           <Route path="/delivery" element={<h2>Delivery</h2>} />
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="Home">
+      <h1>Welcome to my cosmetics store</h1>
+      <p>this is the best decorative cosmetics store for your soul</p>
+      <p>
+        We create only high-quality products so that you can empihasize your
+        beauty
+      </p>
+    </div>
+  );
+}
