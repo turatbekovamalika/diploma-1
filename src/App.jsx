904d231e5b24de79d7171dad7100e3716122b130
Turatbekova Malika <m18049687>
2025-05-18 13:22:06 +0600

    added a route path for element h2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Route, Router, Routes } from "react-router-dom";
 import "./App.css";
 export default function App() {
   return (
@@ -10,6 +10,12 @@ export default function App() {
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
       </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<h2>Home</h2>} />
+          <Route path="/about" element={<h2>About</h2>} />
+        </Routes>
+      </main>
     </div>
   );
 }
