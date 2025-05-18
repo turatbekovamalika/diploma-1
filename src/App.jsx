5b5df6672b124f7ea2fe91c5dd5b0b3c4acedb8e
Turatbekova Malika <m18049687>
2025-05-18 13:37:46 +0600

    Added Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
-import { NavLink, Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
 import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
-      <p>This is my diploma</p>
-
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/contacts">Contacts</NavLink>
-      </nav>
-      <main>
+      <Layout>
         <Routes>
           <Route path="/" element={<h2>Home</h2>} />
           <Route path="/about" element={<h2>About</h2>} />
           <Route path="/contacts" element={<h2>Contacts</h2>} />
         </Routes>
-      </main>
+      </Layout>
     </div>
   );
 }
