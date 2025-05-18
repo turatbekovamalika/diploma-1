94a8f8acf658bb5fb6f6b8d61fdf8a6ff1bf189f
Turatbekova Malika <m18049687>
2025-05-18 13:23:03 +0600

    added route for contacts

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,13 @@ export default function App() {
       <nav>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
+        <NavLink to="/contacts">Contacts</NavLink>
       </nav>
       <main>
         <Routes>
           <Route path="/" element={<h2>Home</h2>} />
           <Route path="/about" element={<h2>About</h2>} />
+          <Route path="/contacts" element={<h2>Contacts</h2>} />
         </Routes>
       </main>
     </div>
