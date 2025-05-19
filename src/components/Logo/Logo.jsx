f8f5baf21a64aac5f171e4a8c97edde28a5da49a
Turatbekova Malika <m18049687>
2025-05-19 20:47:35 +0600

    added logo with navlink

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,13 @@
 import "./Logo.css";
 import logo from "../../assets/logo.svg";
+import { NavLink } from "react-router-dom";
 
 export default function Logo() {
   return (
     <div className="Logo">
-      <img src={logo} alt="Website logo" />
+      <NavLink to="/">
+        <img src={logo} alt="Website logo" />
+      </NavLink>
     </div>
   );
 }
