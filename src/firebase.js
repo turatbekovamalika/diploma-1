5f8b17cecfb953a77f4e3bad8819e47efc68931c
Turatbekova Malika <m18049687>
2025-05-24 12:00:41 +0600

    got a list of categories (collection of documents)

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { collection, getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,3 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+//получить список категорий (коллекция документов)
+export const categoryCollection = collection(db, "categories");
