db42c91768697e054cfe8c0e3b67a5f77c7e50f5
Turatbekova Malika <m18049687>
2025-05-24 12:03:48 +0600

    exported categories and created an array for them

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,6 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 import "./CategoryList.css";
 
 export default function CategoryList() {
+  const [categories, setCategories] = usestate([]);
+  const output = categories.map((category) => (
+    <li key={category.id}>
+      <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
+    </li>
+  ));
+
   return (
     <div className="CategoryList">
       <ul></ul>
