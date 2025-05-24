b2023469ac14d7ba3b756dec2da3e3a2f786ded9
Turatbekova Malika <m18049687>
2025-05-24 12:05:42 +0600

    wrote a useeffect in getdocs with then for const

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,9 +1,28 @@
 import { NavLink } from "react-router-dom";
 
 import "./CategoryList.css";
+import { useEffect, useState } from "react";
+import { getDocs } from "firebase/firestore";
+import { categoryCollection } from "../../firebase";
 
 export default function CategoryList() {
-  const [categories, setCategories] = usestate([]);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getDocs(categoryCollection).then((snapshot) => {
+      const newCategories = [];
+
+      snapshot.docs.forEach((doc) => {
+        const category = doc.data();
+        category.id = doc.id;
+
+        newCategories.push(category);
+      });
+
+      setCategories(newCategories);
+    });
+  }, []);
+
   const output = categories.map((category) => (
     <li key={category.id}>
       <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
@@ -12,7 +31,7 @@ export default function CategoryList() {
 
   return (
     <div className="CategoryList">
-      <ul></ul>
+      <ul> {output}</ul>
     </div>
   );
 }
