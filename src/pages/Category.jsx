54647639c4a3b2890c6bb4111b61b6f5d5584e4e
Turatbekova Malika <m18049687>
2025-05-25 13:25:01 +0600

    category => params.path === category.path

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -5,6 +5,8 @@ import { AppContext } from "../App";
 export default function Category() {
   const { params } = useMatch("/category/:path");
   const { categories } = useContext(AppContext);
+
+  const category = categories.find((category) => params.path === category.path);
   return (
     <div className="Category">
       <h1>Category</h1>
