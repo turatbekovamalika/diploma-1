0cbf3342021af5a9830b961e186d9469cc44af18
Turatbekova Malika <m18049687>
2025-05-25 23:20:43 +0600

    delate img about logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,7 @@ export default function Home() {
   return (
     <div className="Home">
       <h1>Welcome to my cosmetics store</h1>
-      <img
-        src="https://cdn.shopify.com/s/files/1/0662/4598/4498/files/KYLASH-VOLUME-MASCARA_KYLIE_desktop_1728x969_ratio_1920x1077_crop_center.jpg?v=1680548892"
-        alt="photo-kylie"
-      />
+
       <p>this is the best decorative cosmetics store for your soul</p>
       <p>
         We create only high-quality products so that you can empihasize your
