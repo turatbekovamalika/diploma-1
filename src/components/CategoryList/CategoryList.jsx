4d74efc3da0f2d4af75429e39ad77f824a35f5c0
Turatbekova Malika <m18049687>
2025-05-25 15:31:13 +0600

    began our lesson

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,23 +1,27 @@
 import { NavLink } from "react-router-dom";
-
+import { useContext } from "react";
 import "./CategoryList.css";
-import { useContext, useEffect, useState } from "react";
-import { getDocs } from "firebase/firestore";
-import { categoryCollection } from "../../firebase";
 import { AppContext } from "../../App";
 
 export default function CategoryList() {
   const { categories } = useContext(AppContext);
-
-  const output = categories.map((category) => (
-    <li key={category.id}>
-      <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
-    </li>
-  ));
+  categories.sort((a, b) => b.weight - a.weight);
+  const output = categories
+    .sort((a, b) => a.weight - b.weight)
+    .map((category) => (
+      <li key={category.id}>
+        <NavLink
+          to={"/category/" + category.path}
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          {category.name}
+        </NavLink>
+      </li>
+    ));
 
   return (
     <div className="CategoryList">
-      <ul> {output}</ul>
+      <ul>{output}</ul>
     </div>
   );
 }
