bede07c53eceef13a66208a351bec724f9432376
Turatbekova Malika <m18049687>
2025-05-25 20:10:20 +0600

    did a function for product jsx

---
+++ src/pages/Product.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { useMatch } from "react-router-dom";
+import { AppContext } from "../App";
+import NotFound from "./NotFound";
+
+export default function Product() {
+  const { params } = useMatch("/product/:path");
+  const { products } = useContext(AppContext);
+
+  const product = products.find((products) => product.path === params.path);
+
+  if (!product) {
+    return <NotFound />;
+  }
+
+  return (
+    <div className="Product">
+      <h1>{product.name}</h1>
+      <img src={product.picture} alt={product.name} />
+      <span>{product.price} som</span>
+      <AddToCart product={product} />
+    </div>
+  );
+}
