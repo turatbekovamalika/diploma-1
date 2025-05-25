91d6de0a3bafd452bc4fe4247bf29c18a92e15d2
Turatbekova Malika <m18049687>
2025-05-25 18:43:39 +0600

    made an import for links in navigation

--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -1,1 +1,22 @@
 import "./CartList.css";
+import { useContext } from "react";
+import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
+
+export default function CartList() {
+  const { products, cart, setCart } = useContext(AppContext);
+
+  const productIds = Object.keys(cart);
+  const output = products
+    .filter((product) => productIds.includes(product.id))
+    .map((product) => (
+      <div className="CartItem">
+        <img src={product.picture} alt={product.name} />
+        <Link to={"/product/" + product.slug}>{product.name}</Link>
+        <span>{cart[product.id]} items</span>
+        <span>{product.price * cart[product.id]} som</span>
+      </div>
+    ));
+
+  return <div className="CartList">{output}</div>;
+}
