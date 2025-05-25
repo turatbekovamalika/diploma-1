ac2fe913b37aeeee5dc1de76ee770729fe04744f
Turatbekova Malika <m18049687>
2025-05-25 18:37:34 +0600

    cartlink placed in layout

--- src/components/CartLink/CartLink.jsx
+++ src/components/CartLink/CartLink.jsx
@@ -1,1 +1,16 @@
 import "./CartLink.css";
+import { useContext } from "react";
+import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
+
+export default function CartLink() {
+  const { cart } = useContext(AppContext);
+
+  const total = Object.values(cart).reduce((acc, num) => acc + num, 0);
+
+  return (
+    <div className="CartLink">
+      <Link to="/cart">Cart ({total})</Link>
+    </div>
+  );
+}
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,3 +1,4 @@
+import CartLink from "../CartLink/CartLink";
 import CategoryList from "../CategoryList/CategoryList";
 import Logo from "../Logo/Logo";
 
@@ -10,6 +11,7 @@ export default function Layout(props) {
       <header>
         <Nav />
         <Logo />
+        <CartLink />
       </header>
       <main>{props.children}</main>
 
