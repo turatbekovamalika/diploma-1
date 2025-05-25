ef09ca0ffee037faa1c2bc4772a17db53f938801
Turatbekova Malika <m18049687>
2025-05-25 14:10:38 +0600

    completely changed the picture and its content in the product

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -9,12 +9,15 @@ export default function ProductList() {
   const output = products.map((product) => (
     <div className="Product" key={product.id}>
       <img src={product.picture} alt={product.name} />
-      <p>forever and always</p>
+      <div className="Tried">
+        {" "}
+        <p>forever and always</p>
+        <span>{product.price} $</span>
+      </div>
       <Link className="About-product" to={"/product/" + product.path}>
         {" "}
         {product.name}
       </Link>
-      <span>{product.price} $</span>
     </div>
   ));
   return <div className="ProductList">{output}</div>;
