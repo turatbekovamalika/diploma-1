d9b5bda967a604f249896a2c191139fba1190ac1
Turatbekova Malika <m18049687>
2025-05-25 22:57:59 +0600

    export default function Cart in cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,11 @@
 import CartList from "../components/CartList/CartList";
-
+import OrderForm from "../components/OrderForm/OrderForm";
 export default function Cart() {
   return (
     <div className="Cart">
       <h1>Cart</h1>
       <CartList />
+      <OrderForm />
     </div>
   );
 }
