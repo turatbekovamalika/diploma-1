294ffc85501f092ec43ee5015650e4ee9789fc8b
Turatbekova Malika <m18049687>
2025-05-25 13:19:00 +0600

    added const params and categories

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,4 +1,10 @@
+import { useContext } from "react";
+import { useMatch } from "react-router-dom";
+import { AppContext } from "../App";
+
 export default function Category() {
+  const { params } = useMatch("/category/:path");
+  const { categories } = useContext(AppContext);
   return (
     <div className="Category">
       <h1>Category</h1>
