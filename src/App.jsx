e4fe1df1e3c0fca5106267abf1da7712896770f3
Turatbekova Malika <m18049687>
2025-05-25 10:30:39 +0600

    framed the layout with a new provider class

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,28 @@ import About from "./pages/About";
 import Contacts from "./pages/Contacts";
 import Delivery from "./pages/Delivery";
 import Category from "./pages/Category";
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AppContext = createContext({
   categories: [],
 });
+
 export default function App() {
+  const [categories, setCategories] = useState([]);
+
   return (
     <div className="App">
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/delivery" element={<Delivery />} />
-          <Route path="/category/:path" element={<Category />} />
-        </Routes>
-      </Layout>
+      <AppContext.Provider value={{ categories }}>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/delivery" element={<Delivery />} />
+            <Route path="/category/:path" element={<Category />} />
+          </Routes>
+        </Layout>
+      </AppContext.Provider>
     </div>
   );
 }
