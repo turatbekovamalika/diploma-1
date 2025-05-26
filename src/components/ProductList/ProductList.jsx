9b85eb5454b1489b9e7b08b3caa1eb7191c81add
Turatbekova Malika <m18049687>
2025-05-26 09:17:21 +0600

    delate products sort fixed for good

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,11 +1,16 @@
 import { useContext } from "react";
-
 import "./ProductList.css";
 import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
 import AddToCart from "../AddToCart/AddToCart";
+
 export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
+  products.sort((a, b) => {
+    const weightA = a.weight ?? 0;
+    const weightB = b.weight ?? 0;
+    return weightB - weightA;
+  });
 
   const output = products
     .filter((product) => product.category === category.id)
