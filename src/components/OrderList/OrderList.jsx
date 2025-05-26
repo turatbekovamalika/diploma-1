09fcc353ce53b36026cc166ab2eb6af1458b3cf7
Turatbekova Malika <m18049687>
2025-05-26 09:08:55 +0600

    did a paste from telegram

--- src/components/OrderList/OrderList.jsx
+++ src/components/OrderList/OrderList.jsx
@@ -1,3 +1,38 @@
+import { useContext } from "react";
+import { AppContext } from "../../App";
+import "./OrderList.css";
+
 export default function OrderList() {
-  return <div className="OrderList"></div>;
+  const { orders, products } = useContext(AppContext);
+
+  if (!orders.length || !products.length) {
+    return "No orders found.";
+  }
+
+  const output = orders.map((order) => {
+    const cart = Object.keys(order.cart).map((productId) => {
+      const product = products.find((product) => product.id === productId);
+      if (!product) {
+        return "Product not found.";
+      }
+
+      return (
+        <div>
+          {product.name}: {order.cart[product.id]} X {product.price} $ ={" "}
+          {order.cart[product.id] * product.price} $
+        </div>
+      );
+    });
+
+    return (
+      <div className="Order">
+        <div>Name: {order.name}</div>
+        <div>Phone: {order.phone}</div>
+        <div>Address: {order.address}</div>
+        <div>Cart: {cart}</div>
+      </div>
+    );
+  });
+
+  return <div className="OrderList">{output}</div>;
 }
