dab953517b59147dfe4edd2a5dd02658e0080926
Turatbekova Malika <m18049687>
2025-05-26 08:46:54 +0600

    delate classname "home-about" and added br for p

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
+import CategoryList from "../components/CategoryList/CategoryList";
+
 export default function Home() {
   return (
     <div className="Home">
-      <h1>Welcome to my cosmetics store</h1>
-
-      <p>this is the best decorative cosmetics store for your soul</p>
-      <p>
-        We create only high-quality products so that you can empihasize your
-        beauty
-      </p>
+      <h1>Welcome to my beauty world</h1>
+      <div className="Home-about">
+        <p>
+          " True beauty lies in pure and good heart
+          <br /> but you can emphasize it thanks to my cosmetics."
+        </p>
+      </div>
     </div>
   );
 }
