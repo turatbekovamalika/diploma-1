4c79f0c272bcdebe2fd90159674c8abd09a22751
Turatbekova Malika <m18049687>
2025-05-26 15:01:04 +0600

    added navlink about category for product list

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,13 +1,22 @@
 import { useContext } from "react";
 import "./ProductList.css";
 import { AppContext } from "../../App";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import AddToCart from "../AddToCart/AddToCart";
 import AddProduct from "../AddProduct/AddProduct";
 import DeleteProduct from "../DeleteProduct/DeleteProduct";
 import CategoryList from "../CategoryList/CategoryList";
 
 export default function ProductList({ category }) {
+  const { categories } = useContext(AppContext);
+
+  const outputt = categories.map((category) => (
+    <li key={category.id}>
+      <NavLink to={`/categories/${category.slug}`}>
+        <span>{category.name}</span>
+      </NavLink>
+    </li>
+  ));
   const { products } = useContext(AppContext);
   products.sort((a, b) => {
     const weightA = a.weight ?? 0;
@@ -39,6 +48,8 @@ export default function ProductList({ category }) {
 
   return (
     <div className="ProductList">
+      <ul className="Categoryyy">{outputt} </ul>
+
       {output}
       <AddProduct category={category} />
     </div>
