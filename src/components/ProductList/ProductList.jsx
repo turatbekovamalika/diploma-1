49a69e78ee19ef33d97f002496297ad834e381f4
Turatbekova Malika <m18049687>
2025-05-26 08:35:55 +0600

    rename class name Name-img from ppp

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -12,12 +12,12 @@ export default function ProductList({ category }) {
     .map((product) => (
       <div className="Product" key={product.id}>
         <Link className="About-product" to={"/product/" + product.path}>
-          <div className="Ppp">
+          <div className="Name-img">
             <div className="Product-name"> {product.name}</div>
             <img src={product.picture} alt={product.name} />
           </div>
 
-          <div className="Tried">
+          <div className="Color-price">
             {" "}
             <p>{product.color}</p>
             <span>{product.price} $</span>
