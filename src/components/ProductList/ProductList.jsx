b59e0764b22d9bf631f25c42f7cb9917cc8629d5
Turatbekova Malika <m18049687>
2025-05-26 08:31:21 +0600

    added class name for img and product name

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,27 +4,29 @@ import "./ProductList.css";
 import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
 import AddToCart from "../AddToCart/AddToCart";
-
 export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
+
   const output = products
     .filter((product) => product.category === category.id)
     .map((product) => (
       <div className="Product" key={product.id}>
-        <Link className="About-product" to={"/product/" + product.path}></Link>
-        <img src={product.picture} alt={product.name} />
-
-        <div className="Tried">
-          {" "}
-          <p>{product.color}</p>
-          <span>{product.price} $</span>
-        </div>
-        <AddToCart product={product} />
         <Link className="About-product" to={"/product/" + product.path}>
-          {" "}
-          {product.name}
+          <div className="Ppp">
+            <div className="Product-name"> {product.name}</div>
+            <img src={product.picture} alt={product.name} />
+          </div>
+
+          <div className="Tried">
+            {" "}
+            <p>{product.color}</p>
+            <span>{product.price} $</span>
+          </div>
+
+          <AddToCart product={product} />
         </Link>
       </div>
     ));
+
   return <div className="ProductList">{output}</div>;
 }
