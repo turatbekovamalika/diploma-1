fb90cda0bacfacbfe85d75284ebc965afe42495a
Turatbekova Malika <m18049687>
2025-05-26 08:54:17 +0600

    wrote export function Orderlist

---
+++ src/pages/Orders.jsx
@@ -0,0 +1,10 @@
+import OrderList from "../components/OrderList/OrderList";
+
+export default function Orders() {
+  return (
+    <div className="Orders">
+      <h1>Orders</h1>
+      <OrderList />
+    </div>
+  );
+}
