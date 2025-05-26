648b2b8a23d7893db2ee847dd1bc485319531b6e
Turatbekova Malika <m18049687>
2025-05-26 09:18:29 +0600

    corrected the main picture and added categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,32 @@ import CategoryList from "../components/CategoryList/CategoryList";
 export default function Home() {
   return (
     <div className="Home">
-      <h1>Welcome to my beauty world</h1>
-      <div className="Home-about">
-        <p>
-          " True beauty lies in pure and good heart
-          <br /> but you can emphasize it thanks to my cosmetics."
-        </p>
+      <div className="Kylie">
+        <h1>Welcome to my beauty world</h1>
+        <div className="Home-about">
+          <p>
+            " True beauty lies in pure and good heart
+            <br /> but you can emphasize it thanks to my cosmetics."
+          </p>
+        </div>
       </div>
+
       <aside>
         {" "}
         <CategoryList />{" "}
       </aside>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+        consequatur ratione, mollitia eveniet cupiditate rem vero provident sed
+        perferendis nulla sint nostrum recusandae voluptas! Labore ducimus
+        doloribus sint repudiandae ab.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa temporibus
+        ea voluptatibus, itaque consequuntur est officiis modi dicta, eum
+        quibusdam reiciendis, tempora tenetur inventore alias facere? Beatae
+        quisquam ipsa eos?
+      </p>
     </div>
   );
 }
