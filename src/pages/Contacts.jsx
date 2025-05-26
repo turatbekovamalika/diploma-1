66f75341395706e19571958eff405ab3f7bc2df0
Turatbekova Malika <m18049687>
2025-05-26 10:07:19 +0600

    added classsname row and col-3 with col-6 for my contacts

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,3 +1,29 @@
+import eyes from "../assets/eyes.png";
+import "./Contacts.css";
+
 export default function Contacts() {
-  return <div>0503111381</div>;
+  return (
+    <div className="Contacts">
+      <h1>How can you contact us???</h1>
+      <div className="Row">
+        <div className="Col-3">
+          Our contacts: <br />
+          +996 555 27 09 04 <br />
+          +996 855 47 06 03 <br />
+          +996 645 75 07 22 <br />
+          +996 555 27 09 04
+        </div>
+        <div className="Col-6">
+          <img className="Eyes" src={eyes} alt="photo-eyes" />
+        </div>
+        <div className="Col-3">
+          Our contacts: <br />
+          +996 535 85 44 04 <br />
+          +996 975 36 84 54 <br />
+          +996 435 35 94 07 <br />
+          +996 855 47 06 03
+        </div>
+      </div>
+    </div>
+  );
 }
