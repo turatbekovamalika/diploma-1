8e1a477bafe9b964b9227dcf46a235a1bf4af77e
Turatbekova Malika <m18049687>
2025-05-26 10:11:51 +0600

    added imports for category

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,29 @@ import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 import Product from "./pages/Product";
 import ThankYou from "./pages/ThankYou";
-import About from "./pages/About";
 import Contacts from "./pages/Contacts";
+import About from "./pages/About";
 import Delivery from "./pages/Delivery";
 import Orders from "./pages/Orders";
 
 import { createContext, useEffect, useState } from "react";
-import { getDocs } from "firebase/firestore";
 import {
-  categoryCollection,
   onAuthChange,
-  orderCollection,
-  productCollection,
+  onCategoriesLoad,
+  onOrdersLoad,
+  onProductsLoad,
 } from "./firebase";
 
 export const AppContext = createContext({
   categories: [],
   products: [],
   orders: [],
+
   cart: {},
+  setCart: () => {},
+
   user: null,
+  setUser: () => {},
 });
 
 export default function App() {
@@ -33,6 +36,7 @@ export default function App() {
   const [products, setProducts] = useState([]);
   const [orders, setOrders] = useState([]);
   const [user, setUser] = useState(null);
+
   const [cart, setCart] = useState(() => {
     return JSON.parse(localStorage.getItem("cart")) || {};
   });
@@ -42,60 +46,44 @@ export default function App() {
   }, [cart]);
 
   useEffect(() => {
-    getDocs(categoryCollection).then((snapshot) => {
-      const newCategories = snapshot.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
-      setCategories(newCategories);
-    });
-  }, []);
+    onCategoriesLoad(setCategories);
+    onProductsLoad(setProducts);
+    onOrdersLoad(setOrders);
 
-  useEffect(() => {
-    getDocs(productCollection).then((snapshot) => {
-      const newProducts = snapshot.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
-      setProducts(newProducts);
+    onAuthChange((user) => {
+      if (user) {
+        user.isAdmin = user.email === "ajarka1334@gmail.com";
+      }
+      setUser(user);
     });
-  }, []);
-
-  useEffect(() => {
-    getDocs(orderCollection).then((snapshot) => {
-      const newOrders = snapshot.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
-      setOrders(newOrders);
-    });
-  }, []);
-
-  useEffect(() => {
-    const unsubscribe = onAuthChange(setUser);
-    return () => unsubscribe();
   }, []);
 
   return (
     <AppContext.Provider
-      value={{ categories, products, cart, setCart, user, orders }}
+      value={{
+        categories,
+        products,
+        orders,
+        cart,
+        setCart,
+        user,
+        setUser,
+      }}
     >
-      <div className="App">
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contacts" element={<Contacts />} />
-            <Route path="/delivery" element={<Delivery />} />
-            <Route path="/category/:path" element={<Category />} />
-            <Route path="/product/:path" element={<Product />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/orders" element={<Orders />} />
-            <Route path="/thank-you" element={<ThankYou />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
-      </div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/delivery" element={<Delivery />} />
+          <Route path="/category/:path" element={<Category />} />
+          <Route path="/product/:path" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/thank-you" element={<ThankYou />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </AppContext.Provider>
   );
 }
