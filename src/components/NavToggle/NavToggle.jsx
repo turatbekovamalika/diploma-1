e221e0be478ac32ca88283132dfcc727348a6d14
Turatbekova Malika <m18049687>
2025-05-26 10:48:01 +0600

    added function NavToggle for button class name line

---
+++ src/components/NavToggle/NavToggle.jsx
@@ -0,0 +1,13 @@
+import "./NavToggle.css";
+
+function NavToggle({ callback }) {
+  return (
+    <button onClick={callback} className="NavToggle">
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+    </button>
+  );
+}
+
+export default NavToggle;
