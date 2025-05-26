57ab5c57674b20a5c3f4ecf350c088a00c1fecf8
Turatbekova Malika <m18049687>
2025-05-26 09:38:44 +0600

    added new photo for about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,10 @@
+import make from "../assets/make.jpg";
+
 export default function About() {
   return (
     <div className="About">
       <p>this is we</p>
+      <img src={make} alt="" />
     </div>
   );
 }
