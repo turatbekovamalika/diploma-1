b59a4a3cfc26a118e5365bd5239474a46796c8a3
Turatbekova Malika <m18049687>
2025-05-28 09:13:55 +0600

    deleted the inscription about lipstick on the home page in the fourth block

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,21 @@
 import CategoryList from "../components/CategoryList/CategoryList";
 
+import "./Home.css";
+
 export default function Home() {
   return (
     <div className="Home">
       <div className="Kylie">
-        <h1 className="Neon">Welcome to my beauty world</h1>
+        <div className="Container">
+          <h1 className="Neon">Welcome to my beauty world</h1>
 
-        <div className="Home-about">
-          <p>
-            "True beauty lies in pure and good <br />
-            heart but you can emphasize it <br />
-            thanks to my cosmetics."
-          </p>
+          <div className="Home-about">
+            <p>
+              "True beauty lies in pure and good <br />
+              heart but you can emphasize it <br />
+              thanks to my cosmetics."
+            </p>
+          </div>
         </div>
       </div>
 
@@ -20,14 +24,13 @@ export default function Home() {
           <CategoryList />
         </div>
       </aside>
-
       <div className="Kylie-lips">
-        {" "}
-        <p>
-          the best products <br /> only with us
-        </p>
+        <div className="Container">
+          <p>
+            the best products <br /> only with us
+          </p>
+        </div>
       </div>
-
       <div className="Row">
         <div className="Container">
           <div className="Col-6">
@@ -37,7 +40,6 @@ export default function Home() {
               yourself even more.
             </p>
           </div>
-
           <div className="Col-6">
             <p>
               so beauties choose high-quality, good and most importantly at the
@@ -46,26 +48,11 @@ export default function Home() {
           </div>
         </div>
       </div>
-
       <div className="Lip-liner">
         <div className="Row"></div>
       </div>
 
-      <div className="Lipcombo">
-        <div className="Row">
-          <div className="Col-6"></div>
-
-          <div className="Col-6">
-            <p>
-              my crème lipstick is a creamy, comfortable formula that delivers a
-              color-rich, satin sheen finish. featuring a non-sticky and
-              lightweight texture, it effortlessly glides on without feathering
-              or settling into lip lines. this formula hydrates and moisturizes
-              for 8 hours, leaving lips instantly soft.
-            </p>
-          </div>
-        </div>
-      </div>
+      <div className="Choose">Choose the best</div>
     </div>
   );
 }
