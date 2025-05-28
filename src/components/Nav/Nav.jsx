d36232bab2c8ae67eb9a9ff080ccba2e7bb63c00
Turatbekova Malika <m18049687>
2025-05-28 08:45:55 +0600

    swapped the container with navigation until it got to the left

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,24 +4,26 @@ import "./Nav.css";
 export default function Nav() {
   return (
     <div className="Nav">
-      <ul>
-        <li>
-          {" "}
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          {" "}
-          <NavLink to="/about">About</NavLink>
-        </li>
-        <li>
-          {" "}
-          <NavLink to="/contacts">Contacts</NavLink>
-        </li>
-        <li>
-          {" "}
-          <NavLink to="/delivery">Delivery</NavLink>
-        </li>
-      </ul>
+      <div className="Container">
+        <ul>
+          <li>
+            {" "}
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            {" "}
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            {" "}
+            <NavLink to="/contacts">Contacts</NavLink>
+          </li>
+          <li>
+            {" "}
+            <NavLink to="/delivery">Delivery</NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
